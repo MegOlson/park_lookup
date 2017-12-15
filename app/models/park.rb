f1d39ed155a9e8b48d5165d07627ea8804199ff8
mo <meganannetteolson@yahoo.com>
2017-12-15 08:54:06 -0800

    Pass tests for parks get endpoints and create model scope in parks.

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,4 +1,7 @@
 class Park < ApplicationRecord
   belongs_to :state
   validates :name, :presence => true
+
+  scope :by_name, -> (name){ where "name = ?", name}
+  scope :parks, -> (state_id){ where "state_id = ?", state_id }
 end
---
+++ spec/requests/get_parks_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe "get all parks from single state" do
+  let!(:states) { FactoryBot.create(:state) }
+  let!(:parks) { FactoryBot.create_list(:park, 20, state: states) }
+
+  before { get "/api/v1/states/#{states.id}/parks", headers: {Authorization: ENV['PARK_LOOKUP_API']}}
+
+  it 'returns all parks for a state' do
+    expect(JSON.parse(response.body).size).to eq 20
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+end
+
+describe "get single park from single state" do
+  let!(:states) { FactoryBot.create(:state) }
+  let!(:parks) { FactoryBot.create(:park, state: states) }
+
+  before { get "/api/v1/states/#{states.id}/parks/#{parks.id}", headers: {Authorization: ENV['PARK_LOOKUP_API']}}
+
+  it 'returns one parks for one state' do
+    expect(JSON.parse(response.body).size).to eq 3
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+end
